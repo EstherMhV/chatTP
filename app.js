36461d4f9c570e7554b4b75dde1f43f17d9787c5
Arisu Ryohei <carole.2002@outlook.com>
2024-01-15 14:44:29 +0100

    strcuture

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const swaggerUi = require("swagger-ui-express");
+
+const port = 3000;
+const app = express();
+
+const swaggerSpec = require("./src/docs/swagger-config.js");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const swaggerSpec = require("./src/docs/swagger-config.js");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+mongoose.connect("mongodb://0.0.0.0:27017/chat");
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Listening on : ${port}`));
